a52eb41ab3e999b8f3c7361d903a84357f92d44c
Luis Chang <luis.chang@outlook.com>
2025-05-30 21:14:42 -0500

    Quasar

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hola Mundo, soy Login</h1>
+  <input v-model="email" type="text" placeholder="Email" required />
+  <input v-model="password" type="password" placeholder="Password" required />
+
+  <button @click="validate">Login</button>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    validate() {
+      console.log('Click del botón....... Email: ' + this.email)
+    },
+  },
+}
+</script>
+
+<style></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,20 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      // { path: 'login', component: () => import('src/components/auth/LoginForm.vue') },
+    ],
   },
-
+  {
+    path: '/login',
+    component: () => import('src/components/auth/LoginForm.vue'),
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
